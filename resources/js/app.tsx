471b34de817f6000fbc72746564413fe38192d3b
Muhammad Syaoki Faradisa <syaokifaradisa09.com>
2025-10-30 14:33:58 +0800

    feat: clean up project

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,7 +4,6 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { initializeTheme } from './hooks/use-appearance';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -28,6 +27,3 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
-
-// This will set light / dark mode on load...
-initializeTheme();
---
+++ resources/js/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Head } from '@inertiajs/react';
+
+export default function Home() {
+    return (
+        <>
+            <Head title="Home" />
+            <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+                <h1 className="text-4xl font-bold text-gray-800">Hello World</h1>
+            </div>
+        </>
+    );
+}
