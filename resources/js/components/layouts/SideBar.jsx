a2a4db8b62676274098e750356c7a287874a1188
Muhammad Syaoki Faradisa <syaokifaradisa09.com>
2025-11-07 10:13:54 +0800

    refactor: reorder menu in sidebar

--- resources/js/components/layouts/SideBar.jsx
+++ resources/js/components/layouts/SideBar.jsx
@@ -1,4 +1,4 @@
-import { Building, CalendarDays, GitBranch, Home, MonitorDot, Package, ShoppingCart, Tags } from 'lucide-react';
+import { Building, GitBranch, Home, Package, ShoppingCart, Tags, Users } from 'lucide-react';
 import SidebarLink from './SideBarLink';
 
 export default function Sidebar({ isOpen, setIsOpen }) {
@@ -23,85 +23,82 @@ export default function Sidebar({ isOpen, setIsOpen }) {
                     <div className="flex min-h-0 flex-1 flex-col">
                         <div className="scrollbar-thin scrollbar-thumb-gray-300 dark:scrollbar-thumb-slate-700 scrollbar-track-transparent flex-1 overflow-y-auto">
                             <div className="px-3 py-5">
-                                <div className="py-2">
-                                    <h3 className="text-xs font-medium tracking-wider text-slate-500 dark:text-slate-400">
-                                        Halaman Utama
-                                    </h3>
-                                </div>
-                                <div className="space-y-1">
-                                    <SidebarLink
-                                        name="Dashboard"
-                                        href="/dashboard"
-                                        icon={Home}
-                                    />
-                                    <SidebarLink
-                                        roles={[
-                                            'yantek',
-                                            'head_of_pk_laboratory',
-                                            'supervisor_of_pk_laboratory',
-                                            'admin_pk',
-                                            'officer_pk',
-                                            'head_of_ukpr_laboratory',
-                                            'supervisor_of_ukpr_laboratory',
-                                            'admin_ukpr',
-                                            'officer_ukpr',
-                                            'head_of_sarpras_laboratory',
-                                            'supervisor_of_sarpras_laboratory',
-                                            'admin_sarpras',
-                                            'officer_sarpras',
-                                        ]}
-                                        name="Penjadwalan"
-                                        href="/schedule"
-                                        icon={CalendarDays}
-                                    />
-                                    <SidebarLink
-                                        roles={[
-                                            'yantek',
-                                            'head_of_pk_laboratory',
-                                            'it',
-                                        ]}
-                                        name="Monitong Petugas"
-                                        href="/report/lab/kak/officer-monitoring"
-                                        icon={MonitorDot}
-                                    />
-                                </div>
-                                <div className="py-2 mt-4">
-                                    <h3 className="text-xs font-medium tracking-wider text-slate-500 dark:text-slate-400">
-                                        Bisnis
-                                    </h3>
-                                </div>
-                                <div className="space-y-1">
-                                    <SidebarLink
-                                        name="Bisnis"
-                                        href="/business"
-                                        icon={Building}
-                                    />
-                                    <SidebarLink
-                                        name="Kasir"
-                                        href="/cashier"
-                                        icon={ShoppingCart}
-                                    />
-                                    <SidebarLink
-                                        name="Cabang"
-                                        href="/branches"
-                                        icon={GitBranch}
-                                    />
-                                    <SidebarLink
-                                        name="Kategori"
-                                        href="/categories"
-                                        icon={Tags}
-                                    />
-                                    <SidebarLink
-                                        name="Produk"
-                                        href="/products"
-                                        icon={Package}
-                                    />
-                                    <SidebarLink
-                                        name="Karyawan"
-                                        href="/employees"
-                                        icon={Package} // Using the same icon as products for now
-                                    />
-                                </div>
+                                <nav className="space-y-6">
+                                    <div>
+                                        <div className="py-2">
+                                            <h3 className="text-xs font-medium tracking-wider text-slate-500 dark:text-slate-400">
+                                                Halaman Utama
+                                            </h3>
+                                        </div>
+                                        <div className="space-y-1">
+                                            <SidebarLink
+                                                name="Dashboard"
+                                                href="/dashboard"
+                                                icon={Home}
+                                            />
+                                            <SidebarLink
+                                                name="Kasir"
+                                                href="/cashier"
+                                                icon={ShoppingCart}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="py-2">
+                                            <h3 className="text-xs font-medium tracking-wider text-slate-500 dark:text-slate-400">
+                                                Data Bisnis
+                                            </h3>
+                                        </div>
+                                        <div className="space-y-1">
+                                            <SidebarLink
+                                                name="Bisnis"
+                                                href="/business"
+                                                icon={Building}
+                                            />
+                                            <SidebarLink
+                                                name="Cabang"
+                                                href="/branches"
+                                                icon={GitBranch}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="py-2">
+                                            <h3 className="text-xs font-medium tracking-wider text-slate-500 dark:text-slate-400">
+                                                Data Produk
+                                            </h3>
+                                        </div>
+                                        <div className="space-y-1">
+                                            <SidebarLink
+                                                name="Kategori"
+                                                href="/categories"
+                                                icon={Tags}
+                                            />
+                                            <SidebarLink
+                                                name="Produk"
+                                                href="/products"
+                                                icon={Package}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="py-2">
+                                            <h3 className="text-xs font-medium tracking-wider text-slate-500 dark:text-slate-400">
+                                                SDM
+                                            </h3>
+                                        </div>
+                                        <div className="space-y-1">
+                                            <SidebarLink
+                                                name="Pegawai"
+                                                href="/employees"
+                                                icon={Users}
+                                            />
+                                        </div>
+                                    </div>
+                                </nav>
                             </div>
                         </div>
                         {/* <div className="flex-shrink-0 p-3">
